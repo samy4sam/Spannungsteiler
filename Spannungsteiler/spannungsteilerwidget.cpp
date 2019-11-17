2c25fd85bf44d085476a4d29b686bfb558d6a310
Ivan Inderbitzin <iinderbi@hsr.ch>
2019-11-17 19:50:39 +0100

    changed SpinBox to DoubleSpinBox & edited connenct functions

--- Spannungsteiler/spannungsteilerwidget.cpp
+++ Spannungsteiler/spannungsteilerwidget.cpp
@@ -7,28 +7,31 @@ SpannungsteilerWidget::SpannungsteilerWidget(QWidget* parent)
   ui->setupUi(this);
 
   // **** Interaktives Verhalten initialisieren *** :
+  // qOverload damit Compiler weiss ob double oder String
 
-  connect(ui->U1Box, &QSpinBox::valueChanged, &spannungsteiler,
-          &SpannungsteilerLogik::setVol1);
-  connect(ui->U2Box, &QSpinBox::valueChanged, &spannungsteiler,
-          &SpannungsteilerLogik::setVol2);
-  connect(ui->StromBox, &QSpinBox::valueChanged, &spannungsteiler,
-          &SpannungsteilerLogik::setCur);
+  connect(ui->U1Box, qOverload<double>(&QDoubleSpinBox::valueChanged),
+          &spannungsteiler, &SpannungsteilerLogik::setVol1);
+
+  connect(ui->U2Box, qOverload<double>(&QDoubleSpinBox::valueChanged),
+          &spannungsteiler, &SpannungsteilerLogik::setVol2);
+
+  connect(ui->StromBox, qOverload<double>(&QDoubleSpinBox::valueChanged),
+          &spannungsteiler, &SpannungsteilerLogik::setCur);
+
   connect(ui->EReihe_Box, &QComboBox::currentTextChanged, &spannungsteiler,
           &SpannungsteilerLogik::setSerie);
 
   connect(ui->CalcButton, &QPushButton::clicked, &spannungsteiler,
           &SpannungsteilerLogik::doCalc);
+
   connect(ui->ResetButton, &QPushButton::clicked, &spannungsteiler,
           &SpannungsteilerLogik::reset);
 
-  connect(&, &Counter::valueChanged, ui->countLabel,
-          qOverload<int>(
-              &QLabel::setNum));  // setNum hat zwei Implementationen einmal mit
-                                  // "double" und einmal mit "int"
-                                  // Compiler weist nicht welche davon verwendet
-                                  // werden muss daher brauchts den staticCast
-                                  // mit "int" oder "double" typ.
+  connect(&spannungsteiler, &SpannungsteilerLogik::changedRes1,
+          ui->Widerstand1Box, qOverload<double>(&QDoubleSpinBox::setValue));
+
+  connect(&spannungsteiler, &SpannungsteilerLogik::changedRes2,
+          ui->Widerstand2Box, qOverload<double>(&QDoubleSpinBox::setValue));
 }
 
 SpannungsteilerWidget::~SpannungsteilerWidget()
