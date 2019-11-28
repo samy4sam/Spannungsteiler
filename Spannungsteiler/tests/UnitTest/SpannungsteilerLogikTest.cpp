8dd4f5d1f3b50a45fb4cbabfc0e7a5ab2d4d2d9b
Philip Zellweger <p1zellwe@hsr.ch>
2019-11-28 12:55:28 +0100

    Added tests for getfunctions of resistors

--- Spannungsteiler/tests/UnitTest/SpannungsteilerLogikTest.cpp
+++ Spannungsteiler/tests/UnitTest/SpannungsteilerLogikTest.cpp
@@ -1,3 +1,14 @@
 #include "gtest/gtest.h"
 #include "spannungsteilerlogik.h"
 
+TEST(SpannungsteilerLogikTest, getRes1)
+{
+    SpannungsteilerLogik stl;
+    ASSERT_EQ(stl.getRes1(), 0.0);
+}
+
+TEST(SpannungsteilerLogikTest, getRes2)
+{
+    SpannungsteilerLogik stl;
+    ASSERT_EQ(stl.getRes2(), 0.0);
+}
