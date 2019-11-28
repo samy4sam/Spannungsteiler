4dc9c6b3e095dcdcecffc27480004d9a2899d86a
Philip Zellweger <p1zellwe@hsr.ch>
2019-11-28 13:28:56 +0100

    Added test for selectResistor

--- Spannungsteiler/tests/UnitTest/SpannungsteilerLogikTest.cpp
+++ Spannungsteiler/tests/UnitTest/SpannungsteilerLogikTest.cpp
@@ -22,3 +22,12 @@ TEST(SpannungsteilerLogikTest, calcSerieValue)
     ASSERT_EQ(stl.calcSerieValue(2), 4.7);
 }
 
+TEST(SpannungsteilerLogikTest, selectResistor)
+{
+    SpannungsteilerLogik stl;
+    stl.setSerie(serieList::E3);
+    ASSERT_EQ(stl.selectResistor(15.99), 10.0);
+    ASSERT_EQ(stl.selectResistor(16.0), 22.0);
+    ASSERT_EQ(stl.selectResistor(34.49), 22.0);
+    ASSERT_EQ(stl.selectResistor(34.5), 47.0);
+}
