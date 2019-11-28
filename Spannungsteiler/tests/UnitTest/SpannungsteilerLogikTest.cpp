e627147e6b398c9e33e4f82d1da178ce30ed7f0a
Philip Zellweger <p1zellwe@hsr.ch>
2019-11-28 13:27:42 +0100

    Added test for calcSerieValue

--- Spannungsteiler/tests/UnitTest/SpannungsteilerLogikTest.cpp
+++ Spannungsteiler/tests/UnitTest/SpannungsteilerLogikTest.cpp
@@ -12,3 +12,13 @@ TEST(SpannungsteilerLogikTest, getRes2)
     SpannungsteilerLogik stl;
     ASSERT_EQ(stl.getRes2(), 0.0);
 }
+
+TEST(SpannungsteilerLogikTest, calcSerieValue)
+{
+    SpannungsteilerLogik stl;
+    stl.setSerie(serieList::E3);
+    ASSERT_EQ(stl.calcSerieValue(0), 1.0);
+    ASSERT_EQ(stl.calcSerieValue(1), 2.2);
+    ASSERT_EQ(stl.calcSerieValue(2), 4.7);
+}
+
